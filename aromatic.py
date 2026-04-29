0c977201bf42f250e3c6c457d19ae6ece1ad57ef
TheoV19 <theo.vienne@epfl.ch>
2026-04-29 17:35:46 +0200

    final aromatic

--- aromatic.py
+++ aromatic.py
@@ -3,7 +3,6 @@ from rdkit import Chem
 from rdkit.Chem import Draw
 from rdkit.Chem import AllChem
 from functional_groups import detect_functional_groups
-import pubchempy as pcp
 
 
 
@@ -19,31 +18,28 @@ def detect_aromaticity(smiles: str):
 
     aromatic_atoms = []
     for atom in mol.GetAtoms():
-     if atom.GetIsAromatic():
-      aromatic_atoms.append(atom.GetIdx())
+      if atom.GetIsAromatic():
+        aromatic_atoms.append(atom.GetIdx())
 
     aromatic_bonds = []
     for bond in mol.GetBonds():
-     if bond.GetIsAromatic():
-      aromatic_bonds.append((bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()))
+      if bond.GetIsAromatic():
+        aromatic_bonds.append((bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()))
 
     aromatic_rings = []
     ring_info = mol.GetRingInfo()
 
     for ring in ring_info.AtomRings():
-     if all(mol.GetAtomWithIdx(atom_idx).GetIsAromatic() for atom_idx in ring):
-      aromatic_rings.append(list(ring))
+      if all(mol.GetAtomWithIdx(atom_idx).GetIsAromatic() for atom_idx in ring):
+        aromatic_rings.append(list(ring))
 
-    return {
-        "is_aromatic": len(aromatic_rings) > 0,
-        "number_of_aromatic_rings": len(aromatic_rings),
-        "aromatic_atoms": aromatic_atoms,
-        "aromatic_bonds": aromatic_bonds,
-        "aromatic_rings": aromatic_rings,
-        "functional_groups": detected_groups
-    }
+    print(f"is_aromatic: {len(aromatic_rings)} > 0")
+    print(f"number_of_aromatic_rings: {len(aromatic_rings)}")
+    print(f"aromatic_atoms: {aromatic_atoms}") 
+    print(f"aromatic_bonds: {aromatic_bonds}")
+    print(f"aromatic_rings: {aromatic_rings}")
+    print(f"functional_groups: {detected_groups}")
 
-detect_aromaticity("C1=CC=NC=C1")
+    return aromatic_rings, aromatic_atoms, aromatic_bonds, detected_groups
 
-result = detect_aromaticity("C1=CC=NC=C1")
-print(result)
+detect_aromaticity("CC[C@]1(C[C@@H](C2=C(C1)C(=C3C(=C2O)C(=O)C4=C(C3=O)C=CC=C4OC)O)O[C@H]5C[C@@H]([C@@H]([C@@H](O5)C)O)N)O.Cl")
