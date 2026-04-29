b0f48436c1c1fa16a64f313016a57cc7c177a1d2
TheoV19 <theo.vienne@epfl.ch>
2026-04-29 12:24:13 +0200

    3D drawer function

--- draw3D.py
+++ draw3D.py
@@ -8,7 +8,7 @@ import py3Dmol
 import streamlit.components.v1 as components
 
 def draw_molecule_3d(smiles, style='stick', color='spectrum',
-                     width=600, height=400, bg_color='white'):
+                     width=600, height=400, background='white'):
     mol = Chem.MolFromSmiles(smiles)
     if mol is None:
         raise ValueError(f"Invalid SMILES: {smiles}")
@@ -22,10 +22,9 @@ def draw_molecule_3d(smiles, style='stick', color='spectrum',
     view = py3Dmol.view(width=width, height=height)
     view.addModel(sdf_data, 'sdf')
     view.setStyle({style: {'color': color}})
-    view.setBackgroundColor(bg_color)
+    view.setBackgroundColor(background)
     view.zoomTo()
     
     components.html(view._make_html(), height=height)
 
-draw_molecule_3d('CCO')
-draw_molecule_3d('c1ccccc1', style='sphere', color='blue')
+draw_molecule_3d('CCC(=O)N(C1CCN(CC1)CCC2=CC=CC=C2)C3=CC=CC=C3')
