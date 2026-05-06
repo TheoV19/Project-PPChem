3bfc8b4ed5ab2bfbe6476039535ada1feb0f6c98
TheoV19 <theo.vienne@epfl.ch>
2026-05-06 18:39:59 +0200

    add #

--- code.py
+++ code.py
@@ -15,150 +15,150 @@ from functions.point_groups import find_group
 from functions.nucelo_electro import HSAB_rules, electro_nucleo_sites_hsab
 from functions.highlight_functional_groups import rgb_to_svg_color, add_legend_to_svg, draw_molecule_with_functional_groups
 
-st.title("OrganoMind")
-st.image("image.png", width =500)
+# st.title("OrganoMind")
+# st.image("image.png", width =500)
 
-st.header("Welcom on OrganoMind, your organic chemistry assistant 🧪 using PubChem database ! ")
-st.subheader("You can search informations on any existant molecules by entering its name, formula, smiles, inchi, inchikey:")
-search_type = st.selectbox("Enter:", ["name", "formula", "smiles", "inchi", "inchikey"])
-urequest = st.text_input(f"Enter the {search_type}")
+# st.header("Welcom on OrganoMind, your organic chemistry assistant 🧪 using PubChem database ! ")
+# st.subheader("You can search informations on any existant molecules by entering its name, formula, smiles, inchi, inchikey:")
+# search_type = st.selectbox("Enter:", ["name", "formula", "smiles", "inchi", "inchikey"])
+# urequest = st.text_input(f"Enter the {search_type}")
 
-info = st.multiselect("I want informations about:", 
-                      options=[
-                            "Molecular formula", 
-                            "Molecular weight", 
-                            "IUPAC name", 
-                            "SMILES", 
-                            "CAS", 
-                            "Number of rotable bond", 
-                            "Stereochemistry", "Functional group",
-                            "Aromaticity", "Acidity/Basicity", 
-                            "Nucleophilicity/Electrophilicity", 
-                            "Point groups", "3D drawing"], default=["Molecular formula"])
+# info = st.multiselect("I want informations about:", 
+#                       options=[
+#                             "Molecular formula", 
+#                             "Molecular weight", 
+#                             "IUPAC name", 
+#                             "SMILES", 
+#                             "CAS", 
+#                             "Number of rotable bond", 
+#                             "Stereochemistry", "Functional group",
+#                             "Aromaticity", "Acidity/Basicity", 
+#                             "Nucleophilicity/Electrophilicity", 
+#                             "Point groups", "3D drawing"], default=["Molecular formula"])
 
-def filter_cas(synonyms: list[str])-> list[str]:
-    cas = []
-    for s in synonyms:
-        match = re.match("(\d{2,7}-\d\d-\d)", s)
-        if match:
-            return  match.group(1)
-    return None
+# def filter_cas(synonyms: list[str])-> list[str]:
+#     cas = []
+#     for s in synonyms:
+#         match = re.match("(\d{2,7}-\d\d-\d)", s)
+#         if match:
+#             return  match.group(1)
+#     return None
 
-#User enter name, molecular formula,...
-if urequest:
-    with st.spinner("Searching PubChem..."):
-        results = pcp.get_compounds(urequest, search_type)
-        if results:
-            c = results[0]
-            st.success("Compound found!")
+# #User enter name, molecular formula,...
+# if urequest:
+#     with st.spinner("Searching PubChem..."):
+#         results = pcp.get_compounds(urequest, search_type)
+#         if results:
+#             c = results[0]
+#             st.success("Compound found!")
 
-            if "Molecular formula" in info:
-                st.write("**Formula:**", c.molecular_formula)
+#             if "Molecular formula" in info:
+#                 st.write("**Formula:**", c.molecular_formula)
 
-            if "Molecular weight" in info:
-                st.write("**Molecular Weight:**", c.molecular_weight)
+#             if "Molecular weight" in info:
+#                 st.write("**Molecular Weight:**", c.molecular_weight)
 
-            if "IUPAC name" in info:
-                st.write("**IUPAC Name:**", c.iupac_name)
+#             if "IUPAC name" in info:
+#                 st.write("**IUPAC Name:**", c.iupac_name)
 
-            if "SMILES" in info:
-                st.write("**SMILES:**", c.smiles)
+#             if "SMILES" in info:
+#                 st.write("**SMILES:**", c.smiles)
 
-            if "CAS" in info:
-                st.write("**CAS:**", filter_cas(c.synonyms))
+#             if "CAS" in info:
+#                 st.write("**CAS:**", filter_cas(c.synonyms))
 
-            if "Number of rotable bond" in info:
-                st.write("**Rotatable bonds:**", c.rotatable_bond_count)
+#             if "Number of rotable bond" in info:
+#                 st.write("**Rotatable bonds:**", c.rotatable_bond_count)
 
-            if "Stereochemistry" in info:
-                st.write("**Number of chiral center:**", chiral_center(c.smiles))
-                st.write("**Number of existant isomers:**", find_isomers(c.smiles))
-                color_chiral(c.smiles)
+#             if "Stereochemistry" in info:
+#                 st.write("**Number of chiral center:**", chiral_center(c.smiles))
+#                 st.write("**Number of existant isomers:**", find_isomers(c.smiles))
+#                 color_chiral(c.smiles)
 
-            if "Functional group" in info:
-                st.dataframe(detect_functional_groups(c.smiles))
-                draw_molecule_with_functional_groups(c.smiles)
+#             if "Functional group" in info:
+#                 st.dataframe(detect_functional_groups(c.smiles))
+#                 draw_molecule_with_functional_groups(c.smiles)
 
-            if "Aromaticity" in info:
-                st.dataframe(detect_aromatic(c.smiles))
+#             if "Aromaticity" in info:
+#                 st.dataframe(detect_aromatic(c.smiles))
 
-            if "Acidity/Basicity" in info:
-                df_acidic, df_basic = acid_base_estimate(c.smiles)
-                st.write("**Acidic groups:**")
-                st.dataframe(df_acidic)
-                st.write("**Basic groups:**")
-                st.dataframe(df_basic)
+#             if "Acidity/Basicity" in info:
+#                 df_acidic, df_basic = acid_base_estimate(c.smiles)
+#                 st.write("**Acidic groups:**")
+#                 st.dataframe(df_acidic)
+#                 st.write("**Basic groups:**")
+#                 st.dataframe(df_basic)
 
-            if "Nucleophilicity/Electrophilicity" in info:
-                st.dataframe(electro_nucleo_sites_hsab(c.smiles))
+#             if "Nucleophilicity/Electrophilicity" in info:
+#                 st.dataframe(electro_nucleo_sites_hsab(c.smiles))
 
-            if "Point groups" in info:
-                st.write("**Point group:**", find_group(c.smiles))
+#             if "Point groups" in info:
+#                 st.write("**Point group:**", find_group(c.smiles))
 
-            if "3D drawing" in info:
-                draw_molecule_3d(c.smiles)
+#             if "3D drawing" in info:
+#                 draw_molecule_3d(c.smiles)
         
-        else:
-            st.error("No compound found.")
+#         else:
+#             st.error("No compound found.")
 
-#User draw
-st.subheader("or by drawing it:")
-drawing = st_ketcher()
+# #User draw
+# st.subheader("or by drawing it:")
+# drawing = st_ketcher()
 
-if drawing:
-    with st.spinner("searching PubChem..."):
-        result = pcp.get_compounds(drawing, "smiles")
-        if result:
-            c = result[0]
-            st.success("Compound found!")
-            if "Molecular formula" in info:
-                st.write("**Formula:**", c.molecular_formula)
+# if drawing:
+#     with st.spinner("searching PubChem..."):
+#         result = pcp.get_compounds(drawing, "smiles")
+#         if result:
+#             c = result[0]
+#             st.success("Compound found!")
+#             if "Molecular formula" in info:
+#                 st.write("**Formula:**", c.molecular_formula)
 
-            if "Molecular weight" in info:
-                st.write("**Molecular Weight:**", c.molecular_weight)
+#             if "Molecular weight" in info:
+#                 st.write("**Molecular Weight:**", c.molecular_weight)
 
-            if "IUPAC name" in info:
-                st.write("**IUPAC Name:**", c.iupac_name)
+#             if "IUPAC name" in info:
+#                 st.write("**IUPAC Name:**", c.iupac_name)
 
-            if "SMILES" in info:
-                st.write("**SMILES:**", c.smiles)
+#             if "SMILES" in info:
+#                 st.write("**SMILES:**", c.smiles)
 
-            if "CAS" in info:
-                st.write("**CAS:**", filter_cas(c.synonyms))
+#             if "CAS" in info:
+#                 st.write("**CAS:**", filter_cas(c.synonyms))
 
-            if "Number of rotable bond" in info:
-                st.write("**Rotatable bonds:**", c.rotatable_bond_count)
+#             if "Number of rotable bond" in info:
+#                 st.write("**Rotatable bonds:**", c.rotatable_bond_count)
 
-            if "Stereochemistry" in info:
-                st.write("**Number of chiral center:**", chiral_center(c.smiles))
-                st.write("**Number of existant isomers:**", find_isomers(c.smiles))
-                color_chiral(c.smiles)
+#             if "Stereochemistry" in info:
+#                 st.write("**Number of chiral center:**", chiral_center(c.smiles))
+#                 st.write("**Number of existant isomers:**", find_isomers(c.smiles))
+#                 color_chiral(c.smiles)
 
-            if "Functional group" in info:
-                st.dataframe(detect_functional_groups(c.smiles))
-                draw_molecule_with_functional_groups(c.smiles)
+#             if "Functional group" in info:
+#                 st.dataframe(detect_functional_groups(c.smiles))
+#                 draw_molecule_with_functional_groups(c.smiles)
 
-            if "Aromaticity" in info:
-                st.dataframe(detect_aromatic(c.smiles))
+#             if "Aromaticity" in info:
+#                 st.dataframe(detect_aromatic(c.smiles))
 
-            if "Acidity/Basicity" in info:
-                df_acidic, df_basic = acid_base_estimate(c.smiles)
-                st.write("**Acidic groups:**")
-                st.dataframe(df_acidic)
-                st.write("**Basic groups:**")
-                st.dataframe(df_basic)
+#             if "Acidity/Basicity" in info:
+#                 df_acidic, df_basic = acid_base_estimate(c.smiles)
+#                 st.write("**Acidic groups:**")
+#                 st.dataframe(df_acidic)
+#                 st.write("**Basic groups:**")
+#                 st.dataframe(df_basic)
 
-            if "Nucleophilicity/Electrophilicity" in info:
-                st.dataframe(electro_nucleo_sites_hsab(c.smiles))
+#             if "Nucleophilicity/Electrophilicity" in info:
+#                 st.dataframe(electro_nucleo_sites_hsab(c.smiles))
 
-            if "Point groups" in info:
-                st.write("**Point group:**", find_group(c.smiles))
+#             if "Point groups" in info:
+#                 st.write("**Point group:**", find_group(c.smiles))
 
-            if "3D drawing" in info:
-                draw_molecule_3d(c.smiles)
+#             if "3D drawing" in info:
+#                 draw_molecule_3d(c.smiles)
         
-        else:
-            st.error("No compound found.")
+#         else:
+#             st.error("No compound found.")
 
 
 
