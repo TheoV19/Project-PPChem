034f179b700f07589e8bf0216df5d10292399e09
tolgaseckin <tolga.seckin@epfl.ch>
2026-05-17 18:29:56 +0200

    functional groups impovised

--- tests/test_functional_groups.py
+++ tests/test_functional_groups.py
@@ -1,12 +1,20 @@
 #type: ignore
 import pytest
-from organomind.functional_groups import detect_functional_groups
+from organomind.functional_groups import detect_functional_groups, detect_functional_groups_df
 
 def test_functional_groups_found():
     smiles = "CC(=O)O"
     result = detect_functional_groups(smiles)
     assert "carboxylic acid" in result
     assert result["carboxylic acid"]["amount"] == 1
+
+def test_functional_groups_multiple_found():
+    smiles = "CC(=O)Oc1ccccc1C(=O)O"
+    result = detect_functional_groups(smiles)
+    assert "carboxylic acid" in result
+    assert "ester" in result
+    assert result["carboxylic acid"]["amount"] == 1
+    assert result["ester"]["amount"] == 1   
 
 def test_functional_groups_not_found():
     smiles = "O"
@@ -16,3 +24,14 @@ def test_functional_groups_not_found():
 def test_functional_groups_invalid_smiles():
     with pytest.raises(ValueError):
         detect_functional_groups("Invalid_smiles")
+
+def test_functional_groups_df_found():
+    smiles = "CC(=O)O"
+    result = detect_functional_groups_df(smiles)
+    assert result.iloc[0]["Functional Group"] == "carboxylic acid"
+    assert result.iloc[0]["Count"] == 1
+
+def test_functional_groups_df_not_found():
+    smiles = "O"
+    result = detect_functional_groups_df(smiles)
+    assert result.empty
